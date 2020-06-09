03dc2454c6d0f7f0bb24406aed5758d12d3a5ea6
Zumrut-Vie <ekinci.z@gmail.com>
2020-06-09 11:48:06 +0200

    ex with div

--- intermediate exercise/myscript.js
+++ intermediate exercise/myscript.js
@@ -4,28 +4,39 @@
 	
 //Remove the form from the page
 
- // $("form").submit(function(e) {
+ $("form").submit(function(e) {
 
-	// e.preventDefault($("#myform").remove());
- // });
+	e.preventDefault($("#myform").remove());
+ });
 
 	
 
 //Create 5 new DIVs in the article element
 
 $(document).ready(function() {
-        var newelement = $("article").attr("div"); // store the selected h4 tag with its title attribute value
-        
-     });
+        for ( var i = 1; i <= 5; i++ ) {
+        	$("article").append("<div></div>")
+		} 
+       
+        });
 
-var myItems = [];
-var myList = $( "#myList" );
-for ( var i = 0; i < 100; i++ ) {
-    myItems.push( "<li>item " + i + "</li>" );
-}
-myList.append( myItems.join( "" ) );
 
 //Within the input field change the value to "Search for..."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Add a class name of "box" to each new DIV
 //Change the link to "www.codefactory.wien"	
 	
